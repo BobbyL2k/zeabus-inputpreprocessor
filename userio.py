a4830d8074f125b2055d1041798e37f97ecc56be
BobbyL2k <bobby_nh@hotmail.com>
2016-06-15 15:35:26 +0700

    Using future for userio.py

--- userio.py
+++ userio.py
@@ -1,4 +1,11 @@
 "User Interaction Helper Library"
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+from builtins import input
+from future import standard_library
+standard_library.install_aliases()
 
 def confirm(message_str):
     "Ask confirmation from the console"
@@ -9,3 +16,6 @@ def confirm(message_str):
         elif response.lower() == 'n':
             return False
         print("Please type 'Y' or 'N'")
+
+if __name__ == "__main__":
+    print(confirm("test confirm"))
